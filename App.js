95bcce0e1ac4231f13e035a3f042113a9cc460d2
waspryo <sugimoto@liddell.tokyo>
2020-06-18 22:18:48 +0900

    Props and stayle

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Milk'},
+    {id: uuidv4(), text: 'Eggs'},
+    {id: uuidv4(), text: 'Bread'},
+    {id: uuidv4(), text: 'Juice'},
+  ])
+
   return (
     <View style={styles.container}>
       <Header/>
+      <FlatList
+      data={items}
+      renderItem={({item}) =>
+        <ListItem item={item}/>
+      }/>
     </View>
   );
 }
